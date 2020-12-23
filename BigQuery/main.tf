c707971c2df6e32ed3f06ed05faeb61810792f9f
Stephanie <stephanie.walker@sada.com>
2020-12-22 20:06:44 -0800

    update

--- BigQuery/main.tf
+++ BigQuery/main.tf
@@ -2,10 +2,10 @@ module "bigquery" {
   source  = "terraform-google-modules/bigquery/google"
   version = "4.3.0"
   # insert the 6 required variables here
-  dataset_id                  = "Your-Data-Set-ID"
-  dataset_name                = "Your-Data-Set-Name"
-  description                 = "Here you write down a quick description of your dataset"
+  dataset_id                  = "Your-Data-Set-ID" #Here you can either add the name of the dataset you already have or it creates a new one with the name you added
+  dataset_name                = "Your-Data-Set-Name" #this is the field "friendly_name" and is optional
+  description                 = "Here you write down a quick description of your dataset" #optional and not required
   project_id                  = var.project
   location                    = var.region
-  default_table_expiration_ms = 3600000 # This is where you set the time to live of tables using the dataset in milli seconds
+  
 }
