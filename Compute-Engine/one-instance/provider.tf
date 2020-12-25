28350e39048da1d06da82293a60dd692d7f0466c
Stephanie <stephanie.walker@sada.com>
2020-12-24 23:40:39 -0800

    added one instance

---
+++ Compute-Engine/one-instance/provider.tf
@@ -0,0 +1,7 @@
+#we provide the provider name which is google
+#then we add the variable names of the region and zone
+
+provider "google" {
+  credentials = file("i.json")
+  project     =  var.project
+}
---
+++ Compute-Engine/one-instance/variables.tf
@@ -0,0 +1,4 @@
+#set the project name
+variable "project" { 
+  default = "practice-299503"
+}
