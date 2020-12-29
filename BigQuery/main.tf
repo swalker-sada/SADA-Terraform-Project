cf6b8bc2fa95784d6b708d56effd3e178a023e3c
Stephanie <stephanie.walker@sada.com>
2020-12-28 19:20:56 -0800

    update

--- BigQuery/main.tf
+++ BigQuery/main.tf
@@ -1,11 +1,18 @@
 module "bigquery" {
   source  = "terraform-google-modules/bigquery/google"
-  version = "4.3.0"
-  # insert the 6 required variables here
-  dataset_id                  = "practice-dataset" #Here you can either add the name of the dataset you already have or it creates a new one with the name you added
-  dataset_name                = "dataset-12/2020" #this is the field "friendly_name" and is optional
+  #version = "4.3.0"
+  # Create the dataset
+  dataset_id                  = "practice_dataset" #Here you can either add the name of the dataset you already have or it creates a new one with the name you added
+  dataset_name                = "dataset_12/2020" #this is the field "friendly_name" and is optional
   description                 = "This will be used to practice querying data" #optional and not required
   project_id                  = var.project
   location                    = var.region
+
+}
+ 
+  # Table creation
+resource "google_bigquery_table" "bg-table" {
   
+  dataset_id = "practice_dataset" 
+  table_id   = "practice_table"
 }
